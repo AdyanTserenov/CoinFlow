0a64a9eb03b876fc2919272d71852cc1f14f8bce
Adyan <abtserenov@edu.hse.ru>
2025-05-08 18:40:07 +0300

    some schemas added

--- src/main/java/com/example/coinflow/models/SigninRequest.java
+++ src/main/java/com/example/coinflow/models/SigninRequest.java
@@ -1,9 +1,17 @@
 package com.example.coinflow.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на авторизацию")
 public class SigninRequest {
+    @Schema(description = "Email пользователя",
+            example = "ipetrov@example.com",
+            required = true)
     private String username;
+    @Schema(description = "Пароль для входа",
+            example = "vanya_1984",
+            required = true)
     private String password;
 }
--- src/main/java/com/example/coinflow/models/SignupRequest.java
+++ src/main/java/com/example/coinflow/models/SignupRequest.java
@@ -1,10 +1,21 @@
 package com.example.coinflow.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на регистрацию")
 public class SignupRequest {
+    @Schema(description = "Имя пользователя",
+            example = "petrov_ivan",
+            required = true)
     private String username;
+    @Schema(description = "Email пользователя",
+            example = "ipetrov@example.com",
+            required = true)
     private String email;
+    @Schema(description = "Пароль для входа",
+            example = "vanya_1984",
+            required = true)
     private String password;
 }
